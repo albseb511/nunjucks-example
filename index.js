852aa143222b45f537d88388a0975b5bc3097ec0
albseb511 <albertsebastian511@gmail.com>
2021-01-16 13:33:01 +0530

    added docs

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const express = require('express')
 const app = express()
 const port = 5500
 
-
+// will configure nunjucks with a relative folder called views
 nunjucks.configure('views', {
     autoescape: true,
     express: app
 });
+
+// for / route, it will find index.html in views folder 
+// and provide data in the second argument to the template
 
 app.get('/', function(req, res) {
     res.render('index.html', { username: "ALBERT", 
---
+++ readme.md
@@ -0,0 +1,17 @@
+[Getting started with nunjucks](https://mozilla.github.io/nunjucks/getting-started.html)
+
+setup
+
+```
+npm install
+```
+
+dependencies
+
+```
+nodemon
+nunjucks
+express
+```
+
+modify index.js to see changes in views folder
